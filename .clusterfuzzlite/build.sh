f39faf5fcd60adf43881d8e9ede939e72efec8f3
Ivan Gonzalez <ivgonzalez@beyondtrust.com>
2025-01-14 17:52:49 -0500

    CI: build.sh & Dockerfile updated

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -11,3 +11,18 @@
 # $CXX $CXXFLAGS -std=c++11 -Iinclude \
 #     /path/to/name_of_fuzzer.cc -o $OUT/name_of_fuzzer \
 #     $LIB_FUZZING_ENGINE /path/to/library.a
+
+function compile_fuzzer {
+  path=$1
+  function=$2
+  fuzzer=$3
+
+   # Instrument all Go files relevant to this fuzzer
+  go-fuzz-build -libfuzzer -func $function -o $fuzzer.a $path
+
+   # Instrumented, compiled Go ($fuzzer.a) + fuzzing engine = fuzzer binary
+  $CXX $CXXFLAGS $LIB_FUZZING_ENGINE $fuzzer.a -lpthread -o $OUT/$fuzzer
+}
+
+compile_fuzzer ./fuzzing//managed_accounts FuzzGetManagedAccount managed_account_fuzzer
+compile_fuzzer ./fuzzing/secrets FuzzGetSecret secrets_fuzzer
