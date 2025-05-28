4781329dceb246db6e832bd9d31163c6596b2a39
btfhernandez <133419363+btfhernandez@users.noreply.github.com>
2025-05-28 12:36:54 -0500

    fix: ContactEmail default value for creating managed system by workgroup is invalid (#250)
    * fix: ContactEmail default value for creating managed system by workgroup is invalid
    
    * fix: set api address  as required
    
    * fix: remove needless required validation for access url field
    
    * fix: improve tets case to include access url field

--- api/entities/entities.go
+++ api/entities/entities.go
@@ -378,7 +378,7 @@ type ManagedSystemsByWorkGroupIdDetailsBaseConfig struct {
 	UseSSL                             bool   `json:",omitempty"`
 	PlatformID                         int    `json:",omitempty" validate:"required"`
 	NetBiosName                        string `json:",omitempty" validate:"max=15"`
-	ContactEmail                       string `json:",omitempty" validate:"email,max=1000"`
+	ContactEmail                       string `json:",omitempty" validate:"max=1000"`
 	Description                        string `json:",omitempty" validate:"max=255"`
 	Port                               int    `json:",omitempty"`
 	Timeout                            int    `json:",omitempty"`
@@ -401,7 +401,7 @@ type ManagedSystemsByWorkGroupIdDetailsBaseConfig struct {
 	ChangeFrequencyType                string `json:",omitempty" validate:"oneof=first last xdays"`
 	ChangeFrequencyDays                int    `json:",omitempty" validate:"required_if=ChangeFrequencyType xdays"`
 	ChangeTime                         string `json:",omitempty" validate:"datetime=15:04"`
-	AccessURL                          string `json:",omitempty" validate:"required,url"`
+	AccessURL                          string `json:",omitempty"`
 }
 
 type ManagedSystemsByWorkGroupIdDetailsConfig30 struct {
